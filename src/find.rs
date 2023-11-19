ebb32f0e8e2003073ec7e510f00cab3a5babf57e
Robert <robert.3dgamedev@gmail.com>
2023-11-19 11:10:35 +0100

    efactor: move line parsing int its own module

--- src/find.rs
+++ src/find.rs
@@ -1,5 +1,9 @@
 use super::*;
 
+mod line;
+
+use line::{parse as parse_line, Line};
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Section<'a>
 {
@@ -9,7 +13,7 @@ pub enum Section<'a>
 use Section::*;
 
 #[derive(Parser)]
-#[grammar = "section_grammar.pest"]
+#[grammar = "find/section_grammar.pest"]
 struct Section_Parser
 {
 }
@@ -41,68 +45,11 @@ fn find(code: &str) -> Result<Section_List>
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-enum Line<'a>
-{
-  CODE(&'a str),
-  BEGIN_CODEGEN{marker: Marker<'a>, identifier: &'a str,},
-  END_CODEGEN{marker: Marker<'a>, checksum: &'a str,},
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Marker<'a>
 {
   pub indentation: usize,
   pub before_marker: &'a str,
   pub after_marker: &'a str,
-}
-
-fn parse_line(node: crate::pest::iterators::Pair<Rule>) -> Result<Line>
-{
-  use Line::*;
-  use Rule::{code_line, begin_marker_line, end_marker_line};
-
-  let l = match node.as_rule()
-  {
-    code_line => CODE(node.as_str()),
-    begin_marker_line =>
-    {
-      let (marker, identifier) = parse_marker(node.into_inner());
-      Line::BEGIN_CODEGEN{marker, identifier}
-    }
-    end_marker_line =>
-    {
-      let (marker, checksum) = parse_marker(node.into_inner());
-      Line::END_CODEGEN{marker, checksum}
-    }
-    _ => unimplemented!("{:?}", node.as_rule()),
-  };
-
-  return Ok(l);
-}
-
-fn parse_marker(mut xs: crate::pest::iterators::Pairs<Rule>) -> (Marker, &str)
-{
-  let indentation = xs.next().unwrap();
-  let before_marker = xs.next().unwrap();
-  let identifier = xs.next().unwrap();
-  let after_marker = xs.next().unwrap();
-
-  debug_assert_eq!(indentation.as_rule(), Rule::indentation);
-  let indentation = indentation.as_str().len() as usize;
-
-  debug_assert_eq!(before_marker.as_rule(), Rule::before_marker);
-  let before_marker = before_marker.as_str();
-  
-  let identifier = match identifier.as_rule()
-  {
-    Rule::identifier | Rule::checksum => identifier.as_str(),
-    _ => unreachable!("{}", identifier.as_str()),
-  };
-
-  debug_assert_eq!(after_marker.as_rule(), Rule::after_marker);
-  let after_marker = after_marker.as_str();
-
-  (Marker{indentation, before_marker, after_marker}, identifier)
 }
 
 pub type Result<T=(), E=Error> = std::result::Result<T, E>;
@@ -144,25 +91,6 @@ mod test
     */
 
     Ok(())
-  }
-  
-  #[test]
-  fn lines() -> Result
-  {
-    assert_eq!(parse_line("")?, Line::CODE(""));
-    assert_eq!(parse_line("xyz")?, Line::CODE("xyz"));
-    assert_eq!(parse_line("  // << codegen foo >> let's go!")?, Line::BEGIN_CODEGEN{identifier: "foo", marker: Marker{indentation: 2, before_marker: "// ", after_marker: " let's go!"}});
-    assert_eq!(parse_line("  // << /codegen f00ba >> nice!")?, Line::END_CODEGEN{checksum: "f00ba", marker: Marker{indentation: 2, before_marker: "// ", after_marker: " nice!"}});
-
-    Ok(())
-  }
-
-
-  fn parse_line(code: &str) -> Result<Line>
-  {
-    let mut result = Section_Parser::parse(Rule::line, code)?;
-
-    super::parse_line(result.next().unwrap().into_inner().next().unwrap())
   }
 }
 
---
+++ src/find/line.rs
@@ -0,0 +1,110 @@
+use super::*;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Line<'a>
+{
+  CODE(&'a str),
+  BEGIN_CODEGEN{marker: Marker<'a>, identifier: &'a str,},
+  END_CODEGEN{marker: Marker<'a>, checksum: &'a str,},
+}
+
+pub fn parse(node: crate::pest::iterators::Pair<Rule>) -> Result<Line>
+{
+  use Line::*;
+  use Rule::{code_line, begin_marker_line, end_marker_line};
+
+  let l = match node.as_rule()
+  {
+    code_line => CODE(node.as_str()),
+    begin_marker_line =>
+    {
+      let (marker, identifier) = parse_marker(node.into_inner());
+      Line::BEGIN_CODEGEN{marker, identifier}
+    }
+    end_marker_line =>
+    {
+      let (marker, checksum) = parse_marker(node.into_inner());
+      Line::END_CODEGEN{marker, checksum}
+    }
+    _ => unimplemented!("{:?}", node.as_rule()),
+  };
+
+  return Ok(l);
+}
+
+fn parse_marker(mut xs: crate::pest::iterators::Pairs<Rule>) -> (Marker, &str)
+{
+  let indentation = xs.next().unwrap();
+  let before_marker = xs.next().unwrap();
+  let identifier = xs.next().unwrap();
+  let after_marker = xs.next().unwrap();
+
+  debug_assert_eq!(indentation.as_rule(), Rule::indentation);
+  let indentation = indentation.as_str().len() as usize;
+
+  debug_assert_eq!(before_marker.as_rule(), Rule::before_marker);
+  let before_marker = before_marker.as_str();
+  
+  let identifier = match identifier.as_rule()
+  {
+    Rule::identifier | Rule::checksum => identifier.as_str(),
+    _ => unreachable!("{}", identifier.as_str()),
+  };
+
+  debug_assert_eq!(after_marker.as_rule(), Rule::after_marker);
+  let after_marker = after_marker.as_str();
+
+  (Marker{indentation, before_marker, after_marker}, identifier)
+}
+
+#[cfg(test)]
+mod test
+{
+  use super::*;
+  
+  #[test]
+  fn trivial() -> Result
+  {
+    assert_eq!(find("")?, smallvec![HANDWRITTEN("")] as Section_List);
+    assert_eq!(find("xyz")?, smallvec![HANDWRITTEN("xyz")] as Section_List);
+    assert_eq!(find("xyz\nuvw")?, smallvec![HANDWRITTEN("xyz"), HANDWRITTEN("uvw")] as Section_List);
+    /*
+    assert_eq!(find("// << codegen foo >>\n// << /codegen >>\n")?, smallvec![CODEGEN{indentation: 0, ident: "foo", begin:"// << codegen foo >>\n", generated:"", end:"// << /codegen >>\n"}] as Section_List);
+    {
+      /* NOCEHCKIN
+      let code = "xyz\n  // << codegen blub >>\n  uvw\n  // << /codegen >>\nabc";
+      assert_eq!(
+        finder.find(code)?,
+        &[
+          HANDWRITTEN(0..loc(code, 1, 2)),
+          BEGIN_CODEGEN(2, loc(code, 1, 2)..loc(code, 2, 0)),
+          GENERATED(loc(code, 2, 0)..loc(code, 3, 2)),
+          END_CODEGEN(2, loc(code, 3, 2)..loc(code, 4, 0)),
+          HANDWRITTEN(loc(code, 4, 0)..loc(code, 4, 3)),
+        ]);
+        */
+    }
+    */
+
+    Ok(())
+  }
+  
+  #[test]
+  fn lines() -> Result
+  {
+    assert_eq!(parse_line("")?, Line::CODE(""));
+    assert_eq!(parse_line("xyz")?, Line::CODE("xyz"));
+    assert_eq!(parse_line("  // << codegen foo >> let's go!")?, Line::BEGIN_CODEGEN{identifier: "foo", marker: Marker{indentation: 2, before_marker: "// ", after_marker: " let's go!"}});
+    assert_eq!(parse_line("  // << /codegen f00ba >> nice!")?, Line::END_CODEGEN{checksum: "f00ba", marker: Marker{indentation: 2, before_marker: "// ", after_marker: " nice!"}});
+
+    Ok(())
+  }
+
+
+  fn parse_line(code: &str) -> Result<Line>
+  {
+    let mut result = Section_Parser::parse(Rule::line, code)?;
+
+    super::parse_line(result.next().unwrap().into_inner().next().unwrap())
+  }
+}
