c7e13045440090918a44ac89d6f8e54dbb9f383d
Robert <robert.3dgamedev@gmail.com>
2023-11-19 13:14:31 +0100

    refactor

--- src/find.rs
+++ src/find.rs
@@ -1,24 +1,10 @@
 use super::*;
 
+mod section;
+pub use section::{Section, Marker, Section_List};
+
 mod parser;
 pub use parser::parse as find;
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum Section<'a>
-{
-  HANDWRITTEN(&'a str),
-  CODEGEN{indentation: usize, identifier: &'a str, code: &'a str, checksum: Option<blake3::Hash>, begin: Marker<'a>, end: Marker<'a>},
-}
-
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Marker<'a>
-{
-  pub indentation: usize,
-  pub before_marker: &'a str,
-  pub after_marker: &'a str,
-}
-
-pub type Section_List<'a> = SmallVec<[Section<'a> ; 8]>;
 
 pub type Result<T=(), E=Error> = std::result::Result<T, E>;
 
---
+++ src/find/section.rs
@@ -0,0 +1,18 @@
+use super::*;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Section<'a>
+{
+  HANDWRITTEN(&'a str),
+  CODEGEN{indentation: usize, identifier: &'a str, code: &'a str, checksum: Option<blake3::Hash>, begin: Marker<'a>, end: Marker<'a>},
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Marker<'a>
+{
+  pub indentation: usize,
+  pub before_marker: &'a str,
+  pub after_marker: &'a str,
+}
+
+pub type Section_List<'a> = SmallVec<[Section<'a> ; 8]>;
