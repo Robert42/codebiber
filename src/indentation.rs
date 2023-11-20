f88caa0f67039aa98ecb6509053a1e54f9560826
Robert <robert.3dgamedev@gmail.com>
2023-11-20 11:32:47 +0100

    start implementing indentation

--- src/indentation.rs
+++ src/indentation.rs
@@ -1,5 +1,46 @@
+use super::*;
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Indentation(pub usize);
+
+impl Indentation
+{
+  pub fn indent_subrange(self, text: &mut String, rng: std::ops::Range<usize>)
+  {
+    if rng.len() == 0 {return}
+    // TODO: shrink the rng by one if it ends with a linebreak?
+
+    text.reserve(text[rng].as_bytes().iter().copied().filter(|&x| x==b'\n').count() * self.0 + self.0);
+    
+    let indentation = self.to_small_str();
+    let indentation = indentation.as_str();
+    
+    text.insert_str(0, indentation);
+  }
+
+  pub fn indent_string(self, text: &mut String)
+  {
+    self.indent_subrange(text, 0..text.len())
+  }
+
+  pub fn indent_str(self, text: &str) -> String
+  {
+    let mut x = text.to_owned();
+    self.indent_string(&mut x);
+    x
+  }
+
+  pub fn to_small_str(self) -> SmallString::<[u8; 32]>
+  {
+    let mut xs = SmallString::new();
+    xs.reserve(self.0);
+    for _ in 0..self.0
+    {
+      xs.push(' ');
+    }
+    xs
+  }
+}
 
 impl fmt::Display for Indentation
 {
@@ -10,6 +51,30 @@ impl fmt::Display for Indentation
       write!(f, "{}", ' ')?;
     }
     Ok(())
+  }
+}
+
+#[cfg(test)]
+mod test
+{
+  macro_rules! indent {
+    ($i:expr, $str:expr) => {
+      crate::indentation::Indentation($i).indent_str($str).as_str()
+    };
+  }
+
+  #[test]
+  fn test_trivial()
+  {
+    assert_eq!(indent!(0, ""), "");
+    assert_eq!(indent!(0, "x"), "x");
+  }
+
+  #[test]
+  fn test_simpl()
+  {
+    assert_eq!(indent!(2, ""), "");
+    assert_eq!(indent!(2, "x"), "  x");
   }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+extern crate smallstr;
+use smallstr::SmallString;
 #[macro_use]
 extern crate smallvec;
 use smallvec::SmallVec;
