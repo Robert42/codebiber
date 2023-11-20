d2c02861c82bc18a95d7966da7f1fc442d1cef18
Robert <robert.3dgamedev@gmail.com>
2023-11-20 13:56:58 +0100

    Move the line to the and and fill everything before with spaces

--- src/indentation.rs
+++ src/indentation.rs
@@ -5,22 +5,33 @@ pub struct Indentation(pub usize);
 
 impl Indentation
 {
-  pub fn indent_subrange(self, text: &mut String, rng: std::ops::Range<usize>)
+  pub fn indent_subrange(self, text: &mut String, rng: std::ops::RangeFrom<usize>)
   {
-    if rng.len() == 0 {return}
+    if rng.start == text.len() {return}
     // TODO: shrink the rng by one if it ends with a linebreak?
 
-    text.reserve(text[rng].as_bytes().iter().copied().filter(|&x| x==b'\n').count() * self.0 + self.0);
-    
-    let indentation = self.to_small_str();
-    let indentation = indentation.as_str();
-    
-    text.insert_str(0, indentation);
+    let mut bytes = std::mem::take(text).into_bytes();
+
+    let num_linebreaks = bytes[rng.clone()].iter().copied().filter(|&x| x==b'\n').count();
+    let num_indentations = num_linebreaks + 1;
+
+    let src_cursor = bytes.len();
+    let dst_cursor = src_cursor + num_indentations * self.0;
+    bytes.resize(dst_cursor, b'#');
+
+    {
+      let bytes = &mut bytes[rng];
+
+      bytes.copy_within(..src_cursor, dst_cursor-src_cursor);
+      for i in 0..self.0 { bytes[i] = b' '; }
+    }
+
+    *text = unsafe{ String::from_utf8_unchecked(bytes) };
   }
 
   pub fn indent_string(self, text: &mut String)
   {
-    self.indent_subrange(text, 0..text.len())
+    self.indent_subrange(text, 0..)
   }
 
   pub fn indent_str(self, text: &str) -> String
