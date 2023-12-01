ed33ddb7ab60d5610ae693cedd4fd6ee26863991
Robert <robert.3dgamedev@gmail.com>
2023-12-01 22:03:26 +0100

    Use the simpler implemenation

--- src/indentation.rs
+++ src/indentation.rs
@@ -1,5 +1,3 @@
-use super::*;
-
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Indentation(pub usize);
 
@@ -7,9 +5,7 @@ impl Indentation
 {
   pub fn indent_string(self, text: &mut String)
   {
-    let mut bytes = std::mem::take(text).into_bytes();
-
-    indent_lines(&mut bytes, self.0);
+    let bytes = indent_lines(text.as_bytes(), self.0);
 
     *text = unsafe{ String::from_utf8_unchecked(bytes) };
   }
@@ -22,71 +18,7 @@ impl Indentation
   }
 }
 
-struct Line_Indenter<'a>
-{
-  bytes: &'a mut [u8],
-  dst_cursor: usize,
-  src_cursor: usize,
-  indentation: usize,
-}
-
-fn indent_lines(bytes_buf: &mut Vec<u8>, indentation: usize)
-{
-  let start = 0;
-  if indentation == 0 || start == bytes_buf.len() {return;} // microoptimization
-
-  assert!(bytes_buf.len() < u32::MAX as usize);
-
-  let bytes = &bytes_buf[start..];
-  let mut linebreaks : SmallVec<[u32; 32768]> = smallvec![];
-  linebreaks.push(0);
-  for (i,&x) in bytes.iter().enumerate()
-  {
-    if x == b'\n' { linebreaks.push(i as u32+1); }
-  }
-
-  // prevent trailing whitespace
-  {
-    let mut i = 0;
-    let mut j = 0;
-    while j < linebreaks.len()
-    {
-      if j+1 == linebreaks.len() || linebreaks[j]+1 != linebreaks[j+1]
-      {
-        linebreaks[i] = linebreaks[j];
-        i += 1;
-      }
-      j += 1;
-    }
-    linebreaks.truncate(i);
-  }
-  if linebreaks.last().copied() == Some(bytes.len() as u32)
-  {
-    linebreaks.pop();
-  }
-
-  let num_linebreaks = linebreaks.len();
-
-  let src_cursor = bytes.len();
-  let dst_cursor = src_cursor + num_linebreaks * indentation;
-  bytes_buf.resize(start + dst_cursor, b'#');
-
-  let mut indenter = Line_Indenter{
-    bytes: &mut bytes_buf[start..],
-    src_cursor,
-    dst_cursor,
-    indentation,
-  };
-
-  for &i in linebreaks.iter().rev()
-  {
-    indenter.copy_content(i as usize);
-    indenter.fill_indentation();
-  }
-}
-
-#[cfg(test)]
-fn indent_lines_simpl_impl(input: &[u8], indentation: usize) -> Vec<u8>
+fn indent_lines(input: &[u8], indentation: usize) -> Vec<u8>
 {
   let mut output = Vec::with_capacity((input.len()+1)*(indentation+1));
 
@@ -100,30 +32,6 @@ fn indent_lines_simpl_impl(input: &[u8], indentation: usize) -> Vec<u8>
   }
 
   output
-}
-
-impl<'a> Line_Indenter<'a>
-{
-  fn copy_content(&mut self, from: usize)
-  {
-    let n = self.src_cursor - from;
-    let src_rng = from..self.src_cursor;
-    self.src_cursor = from;
-
-    self.dst_cursor -= n;
-    self.bytes.copy_within(src_rng, self.dst_cursor);
-  }
-
-  fn fill_indentation(&mut self)
-  {
-    self.dst_cursor -= self.indentation;
-    fill_with_space(self.bytes, self.dst_cursor..self.dst_cursor+self.indentation);
-  }
-}
-
-fn fill_with_space(bytes: &mut [u8], rng: Range<usize>)
-{
-  for i in rng { bytes[i] = b' '; }
 }
 
 impl fmt::Display for Indentation
@@ -141,9 +49,6 @@ impl fmt::Display for Indentation
 #[cfg(test)]
 mod test
 {
-  use super::indent_lines_simpl_impl;
-  use crate::proptest::prelude::*;
-
   macro_rules! indent {
     ($i:expr, $str:expr) => {
       crate::indentation::Indentation($i).indent_str($str).as_str()
@@ -184,27 +89,12 @@ mod test
     assert_eq!(indent!(2, "\nx"), "\n  x");
   }
 
-  proptest!
-  {
-    #[test]
-    fn prop_same_behavior_as_simple_impl(text in "(\\PC*\\n)*\\PC*", indentation in 0..=255_usize)
-    {
-      let expected = indent_lines_simpl_impl(text.as_bytes(), indentation);
-
-      let mut text = text.into_bytes();
-      super::indent_lines(&mut text, indentation);
-      let actual = text;
-
-      assert_eq!(expected, actual);
-    }
-  }
-
   #[test]
   fn test_indent_lines_simpl_impl()
   {
     fn f(code: &str, indentation: usize) -> String
     {
-      let x = indent_lines_simpl_impl(code.as_bytes(), indentation);
+      let x = super::indent_lines(code.as_bytes(), indentation);
       let x : String = String::from_utf8(x).unwrap();
       x
     }
@@ -218,4 +108,3 @@ mod test
 }
 
 use std::fmt::{self, Write};
-use std::ops::Range;
