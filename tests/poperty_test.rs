c446df4427ece4bf2b7b831fe8954312102a81dc
Robert <robert.3dgamedev@gmail.com>
2023-12-02 22:00:08 +0100

    first trivial comparisont

--- tests/poperty_test.rs
+++ tests/poperty_test.rs
@@ -22,15 +22,31 @@ fn format_input(sections: &[Section], tailing_newline: bool) -> String
       HANDWRITTEN(c) => code += c.as_str(),
     }
   }
-  code
+  set_tailing_linebreak(code, tailing_newline)
+}
+
+fn format_expected_output(sections: &[Section], tailing_newline: bool) -> String
+{
+  let mut code = String::new();
+  for s in sections.into_iter()
+  {
+    match s
+    {
+      HANDWRITTEN(c) => code += c.as_str(),
+    }
+  }
+  set_tailing_linebreak(code, tailing_newline)
 }
 
 proptest!
 {
   #[test]
-  fn roundtrip(sections in many_sections(), tailing_newline: bool)
+  fn roundtrip(sections in many_sections(), hash_bytes in 0..64, tailing_newline: bool)
   {
     let input = format_input(&sections[..], tailing_newline);
+    let expected = format_expected_output(&sections[..], tailing_newline);
+
+    assert_eq!(input, expected);
   }
 }
 
@@ -50,4 +66,19 @@ fn code() -> impl Strategy<Value = String>
       |code| !regex_is_match!("<< *\\/?codegen", &code)
     )
   ]
+}
+
+fn set_tailing_linebreak(mut code: String, expect_tailing_linebreak: bool) -> String
+{
+  let len = code.len();
+  let ends_with_linebreak = code.as_bytes().last().cloned() == Some(b'\n');
+
+  match (expect_tailing_linebreak, ends_with_linebreak)
+  {
+    (false, false) | (true, true) => (),
+    (false, true) => code.truncate(len-1),
+    (true, false) => code.push('\n'),
+  }
+
+  code
 }
