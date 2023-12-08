fd3c918e3db4c1273e85772ea2a6215595233328
Robert <robert.3dgamedev@gmail.com>
2023-12-08 10:52:11 +0100

    MOre examples

--- src/lib.rs
+++ src/lib.rs
@@ -160,6 +160,80 @@ Please not how the generated code in the `bar` section is indented.
 That's because the generated code inherits the indentation of the marker line
 `// << codegen bar >>`
 
+# Example 2
+
+While in this example all marker lines start with `//`, marker lines can start
+and with any characters as long as they don't end with `<` and don't contain
+`<<`.
+
+```rust
+extern crate codebiber;
+
+const INPUT : &str = r"
+/* << codegen foo >> */
+(* << /codegen >> *)
+
+#if 0 // << codegen bar >>
+#endif // << /codegen >>
+
+  ## << codegen bar >> For python like languages
+  -- << /codegen >> Note how you can also write stuff after the marker
+
+    esoteric langugage using keywords << codegen bar >> for comments
+    << /codegen >>
+";
+              
+fn main() -> codebiber::Result
+{
+  let cfg = codebiber::Config{
+    // Anything checksum length other than 0 will catch unintended modifications
+    // since the last modification.
+    checksum_bytes_to_store: 2,
+  };
+
+  let actual_output = codebiber::generate(INPUT, cfg, gen_code_lines)?;
+
+  assert_eq!(actual_output, Some(EXPECTED_OUTPUT.to_owned()));
+
+  Ok(())
+}
+
+fn gen_code_lines(name: &str) -> codebiber::Fmt_Result
+{
+  let generated = match name
+  {
+    "foo" => Some("void autogen_line_foo();".to_owned()),
+    "bar" => Some("void autogen_line_bar1();\nvoid autogen_line_bar2();".to_owned()),
+    _ => None,
+  };
+  Ok(generated)
+}
+
+const EXPECTED_OUTPUT : &str = r"
+/* << codegen foo >> */
+void autogen_line_foo();
+(* << /codegen aaa2 >> *)
+
+#if 0 // << codegen bar >>
+void autogen_line_bar1();
+void autogen_line_bar2();
+#endif // << /codegen 00a2 >>
+
+  ## << codegen bar >> For python like languages
+  void autogen_line_bar1();
+  void autogen_line_bar2();
+  -- << /codegen 00a2 >> Note how you can also write stuff after the marker
+
+    esoteric langugage using keywords << codegen bar >> for comments
+    void autogen_line_bar1();
+    void autogen_line_bar2();
+    << /codegen 00a2 >>
+";
+```
+
+Also note how every `bar` section shares the same hashsum. Thats because the
+hashsum is generated before indenting the code.
+
 */
 
 pub mod parse_file;
