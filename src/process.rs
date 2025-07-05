5bb2a787e2e19cbf65474b8118ad01aacc35b3c4
Robert <robert.hildebrandt@protonmail.com>
2025-07-05 20:58:16 +0200

    Implement simple globbing

--- src/process.rs
+++ src/process.rs
@@ -41,6 +41,68 @@ where F: Fn(&Path, &str) -> Fmt_Result,
   Ok(())
 }
 
+pub fn glob_by_suffix<P: AsRef<Path>>(dir: P, suffixes: &[&str]) -> Result<Vec<PathBuf>, std::io::Error>
+{
+  fn is_extension(p: &Path, s: &str) -> bool
+  {
+    return p.extension().map(|e| e == s).unwrap_or(false)
+  }
+  let is_any_expected_extension = |p: &Path|
+  {
+    return suffixes.into_iter().any(|s| is_extension(p, s))
+  };
+  return glob(dir, &is_any_expected_extension);
+}
+
+/*
+#[test]
+fn glob_sample()
+{
+  let mut s = String::new();
+  let files = glob_by_suffix(".", &["rs"]).unwrap();
+  for p in files.iter()
+  {
+    s.push_str(&format!("{}\n", p.display()));
+  }
+  panic!("== {} ==\n{}", files.len(), s.as_str())
+}
+*/
+
+pub fn glob<P: AsRef<Path>, Pred>(dir: P, pred: &Pred) -> Result<Vec<PathBuf>, std::io::Error>
+  where Pred: Fn(&Path)->bool
+{
+  let dir = dir.as_ref();
+  let mut xs = Vec::new();
+  _visit_dir_glob(&mut xs, dir, pred)?;
+  xs.sort();
+  return Ok(xs);
+}
+
+fn _visit_dir_glob<P>(out: &mut Vec<PathBuf>, dir: &Path, pred: &P) -> std::io::Result<()>
+  where P: Fn(&Path)->bool
+{
+  if dir.is_dir()
+  {
+    for f in fs::read_dir(dir)?
+    {
+      let f = f?.path();
+      let path = f.as_path();
+      if path.is_dir()
+      {
+        _visit_dir_glob(out, path, pred)?;
+      }else if path.is_file()
+      {
+        if pred(&path)
+        {
+          out.push(path.canonicalize()?);
+        }
+      }
+    }
+  }
+
+  Ok(())
+}
+
 pub type Result<T=(), E=Error> = std::result::Result<T, E>;
 
 #[derive(Debug)]
@@ -66,5 +128,7 @@ impl fmt::Display for Process_Error
 impl From<std::io::Error> for Process_Error {fn from(e: std::io::Error) -> Self {Process_Error::IO(e)}}
 impl From<gen::Gen_Error> for Process_Error {fn from(e: gen::Gen_Error) -> Self {Process_Error::GEN(e)}}
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fmt;
+use std::fs;
+
